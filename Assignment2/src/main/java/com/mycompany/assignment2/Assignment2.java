3d83eba10ef4afe152a5fee897d525f6feac3859
saa-lem <60645691+saa-lem@users.noreply.github.com>
2023-10-28 00:32:29 +0300

    First Commit

---
+++ Assignment2/src/main/java/com/mycompany/assignment2/Assignment2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment2;
+
+/**
+ *
+ * @author user
+ */
+public class Assignment2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
